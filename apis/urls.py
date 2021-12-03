42837b71e9a23180ea0cc89380fd6295a64d6906
DebabrataMaity <46934864+DebabrataMaity@users.noreply.github.com>
2021-12-04 00:14:27 +0530

    first commit

---
+++ apis/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+import apis.views as api_view
+from django.contrib import admin
+from . import views
+
+
+urlpatterns = [
+    path('details/', api_view.DetailView.as_view()),
+    path('attendance/', api_view.AttendanceView.as_view()),
+    path('marks/', api_view.MarksView.as_view()),
+    path('timetable/', api_view.TimetableView.as_view()),
+    path('search/', api_view.search, name="search"),
+]
